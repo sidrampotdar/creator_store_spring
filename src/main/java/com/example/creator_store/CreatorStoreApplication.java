38ab4da80edb6e8dbd85da41921a2ccb8ad67b93
Sidramappa Potdar <126233261+sidrampotdar@users.noreply.github.com>
2026-05-19 22:38:05 +0530

    DB Added & env added

--- src/main/java/com/example/creator_store/CreatorStoreApplication.java
+++ src/main/java/com/example/creator_store/CreatorStoreApplication.java
@@ -1,5 +1,6 @@
 package com.example.creator_store;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CreatorStoreApplication {
 
     public static void main(String[] args) {
+
+        // Config for dotenv-java
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry->System.setProperty(entry.getKey(),entry.getValue()));
+
         SpringApplication.run(CreatorStoreApplication.class, args);
     }
 
